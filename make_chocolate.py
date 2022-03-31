6fc94a36008bbb76b0ad68c627a005b66f591037
TesterNick <nikolaevnnne@gmail.com>
2022-03-31 16:14:06 +0300

    Create a stub for main function

---
+++ make_chocolate.py
@@ -0,0 +1,23 @@
+"""
+Task from https://codingbat.com/prob/p190859
+We want make a package of goal kilos of chocolate.
+We have small bars (1 kilo each) and big bars (5 kilos each).
+Return the number of small bars to use, assuming we always use big bars
+before small bars. Return -1 if it can't be done.
+"""
+
+def make_chocolate(small, big, goal):
+    """
+    Evaluate how much small bars should be used, assuming we
+    always use big bars before small bars. Returns -1 if it can't be done.
+
+    :param small: Amount of small bars given.
+    :type small: int
+    :param big: Amount of big bars given.
+    :type big: int
+    :param goal: How much kilos needed.
+    :type goal: int
+    :return: How much small bars can be used, -1 if impossible.
+    :rtype: int
+    """
+    return 0
